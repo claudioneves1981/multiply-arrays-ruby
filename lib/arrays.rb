1e58c860db20157a4a201dcda265da0b69f67be6
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-10-29 17:36:28 -0300

    Multiply Arrays Ruby

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,4 +1,15 @@
 class Arrays
   def self.multiplica_antecessor_predecessor(array)
+    primeiro_numero = array[0] * array[1]
+    ultimo_numero = array[array.length - 2] * array[array.length - 1]
+    array_resultado = []
+    array_resultado.push(primeiro_numero)
+    for a in 0..array.length do
+      if(a+2 < array.length)
+      array_resultado[a+1] = array[a] * array[a+2] 
+      end
+    end
+    array_resultado.push(ultimo_numero)
+    array_resultado
   end
 end
